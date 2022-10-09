ee3d7156b8c543dd77f9cd2efaaf9d010653f726
Aryaman Dubey <aryaman.dubey@kcl.ac.uk>
2022-10-09 09:31:23 +0100

    extends Model not abstract user

--- things/models.py
+++ things/models.py
@@ -1,11 +1,12 @@
 from enum import unique
 from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.db.models import Model
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Thing(AbstractUser):
+class Thing(Model):
     name = models.CharField(max_length = 30, unique = True, blank = False )
     description = models.TextField(unique = False, blank = True, max_length = 120)
     quantity = models.PositiveIntegerField(unique = False, validators=[
