4c4632313f99e418196586111898dec76b39e541
Aryaman Dubey <aryaman.dubey@kcl.ac.uk>
2022-10-09 09:15:08 +0100

    completed exercise 3

--- things/models.py
+++ things/models.py
@@ -1,10 +1,17 @@
+from enum import unique
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Thing(AbstractUser):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.PositiveIntegerField()
-    
+    name = models.CharField(max_length = 30, unique = True, blank = False )
+    description = models.TextField(unique = False, blank = True, max_length = 120)
+    quantity = models.PositiveIntegerField(unique = False, validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ])
+
 
 
